f55d2a7082f524153d4bc1926b3e33a8f7e9a907
Yavnil <ayavnia@gmail.com>
2025-05-22 04:54:08 -0700

    Deleted template function in function_app.py

--- function_app.py
+++ function_app.py
@@ -3,27 +3,6 @@ import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-@app.route(route="resume_http_trigger")
-def resume_http_trigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
-
 @app.route(route="resume_send_and_gather", auth_level=func.AuthLevel.FUNCTION)
 def resume_send_and_gather(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
